49a267b201153e988aa8fa306a8629b6281b1119
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 16:21:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,13 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   @@all = []
+  
   def initialize(title)
     @title = title
+  end
+
+  def GENRES
+    Book.@@all 
   end
 
   def turn_page
